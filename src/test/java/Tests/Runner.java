9b67824507d6df8dd5eeb2f735796e5231bd428c
HassanHusseinM <mahmoud.hassanhussein@vodafone.com>
2022-02-23 02:52:23 +0200

    Task adding report

--- src/test/java/Tests/Runner.java
+++ src/test/java/Tests/Runner.java
@@ -1,13 +1,11 @@
 package Tests;
 
-import Base.TestBase;
+
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
-import org.junit.AfterClass;
 import org.junit.runner.RunWith;
 
-import java.io.File;
 
 @RunWith(Cucumber.class)
 @CucumberOptions( features = "src/test/resources/features",glue = "StepDefinations",plugin = { "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
